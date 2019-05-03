d3dfcfaed7654e08966812c3beda68df1d43a6af
Jon Graf <jongraf1@gmail.com>
2019-05-03 18:25:53 -0500

    Small Straight Will Return Zero When Invalid

---
+++ src/test/java/ScoringServices/SmallStraightTests.java
@@ -0,0 +1,22 @@
+package ScoringServices;
+
+import models.Die;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class SmallStraightTests {
+
+    @Test
+    public void calculateScore_ShouldReturnZeroWhenNotAValidSmallStraight() {
+        SmallStraight straight = new SmallStraight();
+        List<Die> dice = Arrays.asList(new Die(2), new Die(3), new Die(4), new Die(2), new Die(3));
+
+        int actual = straight.calculateScore(dice);
+
+        assertEquals(0, actual);
+    }
+}
