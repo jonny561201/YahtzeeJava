cfdf11e651355f5aa93d0a9db27fb6ff86555ead
Jon Graf <jongraf1@gmail.com>
2019-05-03 18:00:00 -0500

    will return zero points if a large straight isnt valid

---
+++ src/main/java/ScoringServices/LargeStraight.java
@@ -0,0 +1,12 @@
+package ScoringServices;
+
+import models.Die;
+import models.Scoring;
+
+import java.util.List;
+
+public class LargeStraight extends Scoring {
+    public Integer calculateScore(List<Die> dice) {
+        return 0;
+    }
+}
---
+++ src/test/java/ScoringServices/LargeStraightTests.java
@@ -0,0 +1,23 @@
+package ScoringServices;
+
+import models.Die;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class LargeStraightTests {
+
+    @Test
+    public void calculateScore_ShouldReturnZeroIfNotAStraight() {
+        LargeStraight straight = new LargeStraight();
+        List<Die> dice = Arrays.asList(new Die(1), new Die(2), new Die(4), new Die(2), new Die(3));
+
+        int actual = straight.calculateScore(dice);
+
+        assertEquals(0, actual);
+    }
+
+}
