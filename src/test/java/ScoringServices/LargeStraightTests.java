82484f6d7a5e7ef981815ec8dddab53454c06e24
Jon Graf <jongraf1@gmail.com>
2019-05-03 18:55:10 -0500

    Refactored Tests to Extract Common Behavior to Setup

--- src/test/java/ScoringServices/LargeStraightTests.java
+++ src/test/java/ScoringServices/LargeStraightTests.java
@@ -1,6 +1,7 @@
 package ScoringServices;
 
 import models.Die;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Arrays;
@@ -10,9 +11,15 @@ import static org.junit.Assert.assertEquals;
 
 public class LargeStraightTests {
 
+    private LargeStraight straight;
+
+    @Before
+    public void setUp() {
+        straight = new LargeStraight();
+    }
+
     @Test
     public void calculateScore_ShouldReturnZeroIfNotAStraight() {
-        LargeStraight straight = new LargeStraight();
         List<Die> dice = Arrays.asList(new Die(1), new Die(2), new Die(4), new Die(2), new Die(3));
 
         int actual = straight.calculateScore(dice);
@@ -22,7 +29,6 @@ public class LargeStraightTests {
 
     @Test
     public void calculateScore_ShouldReturnFortyIfSequentialUniquePips() {
-        LargeStraight straight = new LargeStraight();
         List<Die> dice = Arrays.asList(new Die(2), new Die(3), new Die(4), new Die(5), new Die(6));
 
         int actual = straight.calculateScore(dice);
@@ -32,7 +38,6 @@ public class LargeStraightTests {
 
     @Test
     public void calculateScore_ShouldReturnZeroIfNonSequentialUniquePips() {
-        LargeStraight straight = new LargeStraight();
         List<Die> dice = Arrays.asList(new Die(1), new Die(3), new Die(4), new Die(5), new Die(6));
 
         int actual = straight.calculateScore(dice);
