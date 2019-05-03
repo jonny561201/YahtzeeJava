f685c21794752580608d148d42fb844d3ff01507
Jon Graf <jongraf1@gmail.com>
2019-05-03 18:41:11 -0500

    small straight will return thirty points when correct combination of two through five

--- src/main/java/ScoringServices/SmallStraight.java
+++ src/main/java/ScoringServices/SmallStraight.java
@@ -3,6 +3,8 @@ package ScoringServices;
 import models.Die;
 import models.Scoring;
 
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -10,6 +12,15 @@ import java.util.stream.Collectors;
 public class SmallStraight extends Scoring {
     public Integer calculateScore(List<Die> dice) {
         Set<Integer> pipSet = dice.stream().map(x -> x.getPips()).collect(Collectors.toSet());
-        return pipSet.size() >= 4 ? 30 : 0;
+        return areValidUniquePips(pipSet) && isSequential(pipSet) ? 30 : 0;
+    }
+
+    private boolean areValidUniquePips(Set<Integer> pipSet) {
+        return pipSet.size() >= 4;
+    }
+
+    private boolean isSequential(Set<Integer> pipSet) {
+        List<Integer> validLow = Arrays.asList(2,3,4,5);
+        return pipSet.containsAll(validLow);
     }
 }
--- src/test/java/ScoringServices/SmallStraightTests.java
+++ src/test/java/ScoringServices/SmallStraightTests.java
@@ -39,4 +39,14 @@ public class SmallStraightTests {
 
         assertEquals(30, actual);
     }
+
+    @Test
+    public void calculateScore_ShouldReturnZeroWhenNonSequentialSetOfFour() {
+        SmallStraight straight = new SmallStraight();
+        List<Die> dice = Arrays.asList(new Die(1), new Die(2), new Die(4), new Die(5), new Die(6));
+
+        int actual = straight.calculateScore(dice);
+
+        assertEquals(0, actual);
+    }
 }
