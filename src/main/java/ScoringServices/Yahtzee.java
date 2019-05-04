a86fcb904537f858ee68ba0adcb0fe259234a37e
Jon Graf <jongraf1@gmail.com>
2019-05-04 17:55:13 -0500

    Yahtzee Returns Fifty Points if Valid Yahtzee

--- src/main/java/ScoringServices/Yahtzee.java
+++ src/main/java/ScoringServices/Yahtzee.java
@@ -4,9 +4,20 @@ import models.Die;
 import models.Scoring;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Yahtzee extends Scoring {
     public Integer calculateScore(List<Die> dice) {
-        return 0;
+
+        int primeValue = dice.get(0).getPips();
+        boolean isValid = true;
+
+        for (Die die : dice) {
+            if (die.getPips() != primeValue) {
+                isValid = false;
+            }
+        }
+
+        return isValid ? 50 : 0;
     }
 }
--- src/test/java/ScoringServices/YahtzeeTests.java
+++ src/test/java/ScoringServices/YahtzeeTests.java
@@ -19,4 +19,14 @@ public class YahtzeeTests {
 
         assertEquals(0, actual);
     }
+
+    @Test
+    public void calculateScore_shouldReturnFiftyPointsForYahtzee() {
+        List<Die> dice = Arrays.asList(new Die(4), new Die(4), new Die(4), new Die(4), new Die(4));
+        Yahtzee yahtzee = new Yahtzee();
+
+        int actual = yahtzee.calculateScore(dice);
+
+        assertEquals(50, actual);
+    }
 }
