7c0ae83579c665edde3ab44368f14c9beed320a8
Jon Graf <jongraf1@gmail.com>
2019-05-08 18:10:05 -0500

    three of a kind will return zero when invalid

---
+++ src/main/java/ScoringServices/ThreeOfAKind.java
@@ -0,0 +1,14 @@
+package ScoringServices;
+
+import models.Die;
+import models.Scoring;
+
+import java.util.List;
+
+public class ThreeOfAKind extends Scoring {
+
+    public Integer calculateScore(List<Die> dice) {
+
+        return 0;
+    }
+}
---
+++ src/test/java/ScoringServices/ThreeOfAKindTests.java
@@ -0,0 +1,23 @@
+package ScoringServices;
+
+import models.Die;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class ThreeOfAKindTests {
+
+    @Test
+    public void calculateScore_shouldReturnZeroForInvalidThreeOfAKind() {
+        ThreeOfAKind threeOfAKind = new ThreeOfAKind();
+        List<Die> dice = Arrays.asList(new Die(1), new Die(2), new Die(3), new Die(4), new Die(5));
+
+        int actual = threeOfAKind.calculateScore(dice);
+
+        assertEquals(0, actual);
+    }
+
+}
